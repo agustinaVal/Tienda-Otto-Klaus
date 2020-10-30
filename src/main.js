d6b01a6739b28a6308740723255dc3e43c779ae1
Agustina <maria.agustina.valenti89@gmail.com>
2020-10-30 16:38:55 -0300

    conectando firebase al proyecto

--- src/main.js
+++ src/main.js
@@ -1,16 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import vuetify from './plugins/vuetify';
-import '@babel/polyfill'
+import '@babel/polyfill';
+import firebase from 'firebase';
 
+var firebaseConfig = {
+	apiKey: 'AIzaSyC1qQDyMjYz288rYUo9QVBt31PBYpGndpo',
+	authDomain: 'prueba-deploy-78473.firebaseapp.com',
+	databaseURL: 'https://prueba-deploy-78473.firebaseio.com',
+	projectId: 'prueba-deploy-78473',
+	storageBucket: 'prueba-deploy-78473.appspot.com',
+	messagingSenderId: '806028368674',
+	appId: '1:806028368674:web:32e2ffd251887c2dbda37b',
+};
 
-Vue.config.productionTip = false
+firebase.initializeApp(firebaseConfig);
 
+Vue.config.productionTip = false;
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	vuetify,
+	render: (h) => h(App),
+}).$mount('#app');
