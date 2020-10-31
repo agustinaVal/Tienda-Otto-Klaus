717c91fd0a4d8eecc533424e13d3326b1fdf4122
Agustina <maria.agustina.valenti89@gmail.com>
2020-10-31 00:56:07 -0300

    agregando data en firestore

--- src/store/Datos/Data.js
+++ src/store/Datos/Data.js
@@ -1,53 +1,66 @@
 import firebase from 'firebase';
 
 export default {
-	namespaced: true,
-	state: {
-		Productos: [],
-	},
-	getters: {},
-	actions: {
-		async getData({ commit }) {
-			const db = await firebase
-				.firestore()
-				.collection('productos')
-				.get();
-			db.forEach((el) => {
-				const producto = {
-					id: el.id,
-					nombre: el.data().nombre,
-					codigo: el.data().codigo,
-					stock: el.data().stock,
-					precio: el.data().precio,
-				};
-				commit('setData', producto);
-			});
-		},
-		async addData({ commit }, payload) {
-			const precio = Number(payload.precio);
-			const stock = Number(payload.stock);
-			const nombre = payload.nombre.toLowerCase();
-			const codigo = payload.codigo.toUpperCase();
+    namespaced: true,
+    state: {
+        Productos: [],
+    },
+    getters: {},
+    actions: {
+        async getData({
+            commit
+        }) {
+            const db = await firebase
+                .firestore()
+                .collection('productos')
+                .get();
+            db.forEach((el) => {
+                const producto = {
+                    id: el.id,
+                    nombre: el.data().nombre,
+                    codigo: el.data().codigo,
+                    stock: el.data().stock,
+                    precio: el.data().precio,
+                };
+                commit('setData', producto);
+            });
+        },
+        async addData({
+            commit
+        }, payload) {
+            const precio = Number(payload.precio);
+            const stock = Number(payload.stock);
+            const nombre = payload.nombre.toLowerCase();
+            const codigo = payload.codigo.toUpperCase();
 
-			const juguete = {
-				precio,
-				stock,
-				nombre,
-				codigo,
-			};
+            const juguete = {
+                precio,
+                stock,
+                nombre,
+                codigo,
+            };
 
-			// agregar firestore
+            // agregar firestore
+           try {
+            await firebase
+            .firestore()
+            .collection('productos')
+            .add(juguete);
+           } catch (error){
+             alert("Algo salio mal, reintente" , error)
+           }
 
-			// agregar a store
-			commit('addData', juguete);
-		},
-	},
-	mutations: {
-		setData(state, payload) {
-			state.Productos.push(payload);
-		},
-		addData(state, payload) {
-			state.Productos.push(payload);
-		},
-	},
+
+            // agregar a store
+            commit('addData', juguete);
+        },
+    },
+    mutations: {
+        setData(state, payload) {
+            state.Productos.push(payload);
+        },
+        addData(state, payload) {
+            state.Productos.push(payload);
+        },
+    },
 };
